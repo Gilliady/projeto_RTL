c4a072fba44bccaac915be954780e78b59da84c2
Gilliady <gilliadyazevedo@gmail.com>
2023-05-02 15:55:48 -0300

    finalizado requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+test('', () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push('/teste/notFound');
+  });
+  screen.getByRole('heading', { name: /page requested not found/i });
+  const img = screen.getByRole('img', { name: /pikachu crying/i });
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
