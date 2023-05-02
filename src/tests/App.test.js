1270c7598a09620f453c0154cabcb3d871835166
Gilliady <gilliadyazevedo@gmail.com>
2023-05-02 17:50:58 -0300

    finalizado requisito 7

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,15 +6,16 @@ import App from '../App';
 
 test('Testando o funcionamento do App.js, rotas: Home, About, Favorites e NotFound', () => {
   const { history } = renderWithRouter(<App />);
+  const { location: { pathname } } = history;
   const home = screen.getByRole('link', { name: /home/i });
   const about = screen.getByRole('link', { name: /about/i });
   const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
   userEvent.click(home);
-  expect(history.location.pathname).toBe('/');
+  expect(pathname).toBe('/');
   userEvent.click(about);
-  expect(history.location.pathname).toBe('/about');
+  expect(pathname).toBe('/about');
   userEvent.click(favorite);
-  expect(history.location.pathname).toBe('/favorites');
+  expect(pathname).toBe('/favorites');
   act(() => {
     history.push('/teste/notFound');
   });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('', () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push('/pokemon/25');
+  });
+
+  screen.getByRole('heading', { name: /pikachu details/i, level: 2 });
+  expect(screen.queryByRole('link', { name: /more details/i })).not
+    .toBeInTheDocument();
+
+  screen.getByRole('heading', { name: /summary/i });
+  screen.getByText(/this intelligent pokémon roasts hard berries with electricity to make them tender enough to eat\./i);
+
+  screen.getByRole('heading', { name: /game locations of pikachu/i });
+
+  screen.getByText(/kanto viridian forest/i);
+  screen.getByText(/kanto power plant/i);
+  const locationsImages = screen.getAllByRole('img', { name: /pikachu location/i });
+  expect(locationsImages).toHaveLength(2);
+
+  expect(locationsImages[0].src).toBe('https://archives.bulbagarden.net/media/upload/0/08/Kanto_Route_2_Map.png');
+
+  expect(locationsImages[1].src).toBe('https://archives.bulbagarden.net/media/upload/b/bd/Kanto_Celadon_City_Map.png');
+
+  userEvent.click(screen.getByText(/pokémon favoritado\?/i));
+  const favoriteIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+  expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
+});
