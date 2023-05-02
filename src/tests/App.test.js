ca55d3f29424da04a2b056e10ac9172f17308412
Gilliady <gilliadyazevedo@gmail.com>
2023-05-02 15:08:25 -0300

    finalizado requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+test('Testando o funcionamento do App.js, rotas: Home, About, Favorites e NotFound', () => {
+  const { history } = renderWithRouter(<App />);
+  const home = screen.getByRole('link', { name: /home/i });
+  const about = screen.getByRole('link', { name: /about/i });
+  const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+  userEvent.click(home);
+  expect(history.location.pathname).toBe('/');
+  userEvent.click(about);
+  expect(history.location.pathname).toBe('/about');
+  userEvent.click(favorite);
+  expect(history.location.pathname).toBe('/favorites');
+  act(() => {
+    history.push('/teste/notFound');
+  });
+  screen.getByRole('heading', { name: /page requested not found/i });
+});
