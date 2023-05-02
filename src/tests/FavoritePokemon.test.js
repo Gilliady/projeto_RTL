3e4baf300f0513c7b4f5226143bb4d653c44112e
Gilliady <gilliadyazevedo@gmail.com>
2023-05-02 15:52:05 -0300

    finalizado requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+test('', async () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push('/favorites');
+  });
+  screen.getByText(/no favorite pokémon found/i);
+  userEvent.click(screen.getByRole('link', { name: /home/i }));
+  userEvent.click(screen.getByRole('link', { name: /more details/i }));
+  userEvent.click(screen.getByText(/pokémon favoritado\?/i));
+  act(() => {
+    history.push('/favorites');
+  });
+  screen.getByText(/pikachu/i);
+});
